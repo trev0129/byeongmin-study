432527719f03c707d54374705a17434ac5f5cf18
trev0129 <pbm9393@gmail.com>
2022-12-03 21:28:34 +0900

    백준문제연습

---
+++ myboard-app/app/src/main/java/com/min/baekjoonQuiz/ex02/Exam1330.java
@@ -0,0 +1,41 @@
+package com.min.baekjoonQuiz.ex02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Exam1330 {
+  public static void main(String[] args) throws IOException {
+    //    Scanner in = new Scanner(System.in);
+    //    int a = in.nextInt();
+    //    int b = in.nextInt();
+    //    in.close();
+    //
+    //    // 1) 일반적인 방법 
+    //    if (a > b) {
+    //      System.out.println(">");
+    //    } else if (a < b) {
+    //      System.out.println("<");
+    //    } else {
+    //      System.out.println("==");
+    //    }
+    //
+    //    // 2) 3항 연산자 활용
+    //    System.out.println((a>b)?">":(a<b)?"<":"==");
+
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    // 3) StringBuffer 활용 - StirngTokenizer
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+    System.out.println((a>b)?">":(a<b)?"<":"==");
+
+
+
+
+
+  } // main
+}
--- myboard-app/app/src/main/java/com/min/quiz/ex02/sol/Test02.java
+++ myboard-app/app/src/main/java/com/min/quiz/ex02/sol/Test02.java
@@ -1,4 +1,4 @@
-package com.eomcs.quiz.ex02.sol;
+package com.min.quiz.ex02.sol;
 
 // 출처: codefights.com
 //

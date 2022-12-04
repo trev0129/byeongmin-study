093f95ef1bf51a5d22cc6f188c090f1c80c2c627
trev0129 <pbm9393@gmail.com>
2022-12-04 22:45:21 +0900

    백준 연습.

---
+++ myboard-app/app/src/main/java/com/min/baekjoonQuiz/ex02/Exam9498.java
@@ -0,0 +1,20 @@
+package com.min.baekjoonQuiz.ex02;
+
+import java.util.Scanner;
+
+public class Exam9498 {
+  public static void main(String[] args) {
+    int grade = (new Scanner(System.in)).nextInt();
+    if (grade>=90) {
+      System.out.println("A");
+    } else if (grade>=80) {
+      System.out.println("B");
+    } else if (grade>=70) {
+      System.out.println("C");
+    } else if (grade>=60) {
+      System.out.println("D");
+    } else {
+      System.out.println("F");
+    }
+  }
+}

f44c4f91221a820e573e407eb021ae0506c09d8b
trev0129 <pbm9393@gmail.com>
2022-12-06 22:26:41 +0900

    백준 연습2단계

---
+++ myboard-app/app/src/main/java/com/min/baekjoonQuiz/ex02/Exam2884.java
@@ -0,0 +1,22 @@
+package com.min.baekjoonQuiz.ex02;
+
+import java.util.Scanner;
+
+public class Exam2884 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int h = in.nextInt();
+    int m = in.nextInt();
+    if (m >= 45) {
+      m -= 45;
+    } else {
+      m += 15;
+      h -= 1;
+      if (h == -1) {
+        h = 23;
+      }
+    }
+    System.out.println(h + " " + m);
+    in.close();
+  }
+}

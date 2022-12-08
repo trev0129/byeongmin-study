fe13e0e1f43d2ea31674e0e2fdc5bbd9e052e1d5
trev0129 <pbm9393@gmail.com>
2022-12-08 13:55:17 +0900

    백준 연습2단계

--- myboard-app/app/src/main/java/com/min/baekjoonQuiz/ex02/Exam02525.java
+++ myboard-app/app/src/main/java/com/min/baekjoonQuiz/ex02/Exam02525.java
@@ -2,18 +2,18 @@ package com.min.baekjoonQuiz.ex02;
 
 import java.util.Scanner;
 
-public class Exam2884 {
+public class Exam02525 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int h = in.nextInt();
     int m = in.nextInt();
-    if (m >= 45) {
-      m -= 45;
-    } else {
-      m += 15;
-      h -= 1;
-      if (h == -1) {
-        h = 23;
+    int t = in.nextInt();
+    m += t;
+    while(m > 59) {
+      m -= 60;
+      h += 1;
+      if (h == 24) {
+        h = 0;
       }
     }
     System.out.println(h + " " + m);
